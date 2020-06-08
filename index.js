c0d334333b160238aa7138a18ed3090e934a9583
Lucas Ocampos <lucas.v.ocampos@gmail.com>
2020-06-08 10:36:15 -0300

    Different approach to role change

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ CLIENT.on('message', message => {
         let nickname = args.join(' ');
 
         message.member.setNickname(nickname);
-        message.member.roles.remove([roleNaoInscrito]);
-        message.member.roles.add([roleEstudante]);
+        message.author.roles.remove([roleNaoInscrito]);
+        message.author.roles.add([roleEstudante]);
     }
 
     if(args[0] === 'signup' && channelID !== canalSignUp){
